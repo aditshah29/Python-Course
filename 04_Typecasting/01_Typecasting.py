94d6899a82ac8d7bcddf38ab3d54375a343fd3e4
aditshah29 <aditshah294@gmail.com>
2025-09-26 10:32:36 +0530

    first commit

---
+++ 04_Typecasting/01_Typecasting.py
@@ -0,0 +1,18 @@
+a=34
+b="34"
+d=223
+
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+
+#Convert b to an integer
+c=int(b) #This will convert b from string to integer.
+print(c)
+print(type(c))
+
+e=str(d)
+print(e)
+print(type(e))
